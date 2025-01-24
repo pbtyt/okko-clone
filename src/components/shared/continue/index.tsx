ec3f5a05e7d8a8f5258e3b33c2f7dd7b2e918cf7
bladee <darkyt9@gmail.com>
2025-01-24 15:30:42 +0300

    shared scroll-item and continue elements have been moved to a separate component

--- src/components/shared/continue/index.tsx
+++ src/components/shared/continue/index.tsx
@@ -7,6 +7,7 @@ import { Ellipsis } from 'lucide-react';
 import { useRef } from 'react';
 import { ContinueDropDown } from '../continue-drop-down';
 import styles from './continue.module.css';
+import { Card } from '../card';
 
 export function Continue() {
 	const { setDropDownPosition, setIsDropDown, isDropDown, setDropDownMenu } =
@@ -26,45 +27,55 @@ export function Continue() {
 		setDropDownMenu(<ContinueDropDown />);
 	};
 	return (
-		<div className={styles.scrollItemWrapper}>
-			<div className={styles.scrollItemPosterWrapper}>
-				<img
-					src='https://static.okko.tv/images/v4/52c18c21-efc9-4c45-807f-476231a02cea?width=319&scale=1&quality=80&mediaType=webp'
-					alt='cw poster'
-					className={styles.scrollItemPoster}
-				/>
-				<div className={styles.progress}></div>
-				<div className={styles.progressFill} style={{ width: '100px' }}></div>
-			</div>
-			<div
-				style={{
-					display: 'flex',
-					width: '100%',
-					justifyContent: 'space-between',
-				}}
-			>
-				<div
-					style={{
-						display: 'flex',
-						flexDirection: 'column',
-						gap: '2px',
-					}}
-				>
-					<span className={styles.title}>Еретик</span>
-					<span className={styles.timePerEpisode}>25 мин</span>
-					<span className={styles.time}>1 час 30 мин</span>
+
+		<Card
+			style={{flexDirection: 'column', background: 'none'}}
+			footer={
+				<div className={styles.footerWrapper}>
+					<div className={styles.progress}></div>
+					<div className={styles.progressFill} style={{ width: '100px' }}></div>
+					<div
+						style={{
+							display: 'flex',
+							width: '100%',
+							justifyContent: 'space-between',
+						}}
+					>
+						<div
+							style={{
+								display: 'flex',
+								flexDirection: 'column',
+								gap: '2px',
+							}}
+						>
+							<span className={styles.title}>Еретик</span>
+							<span className={styles.timePerEpisode}>25 мин</span>
+							<span className={styles.time}>1 час 30 мин</span>
+						</div>
+						<button
+							ref={ref}
+							style={{ alignSelf: 'center' }}
+							onClick={handleOnMoreOptions}
+						>
+							<Ellipsis
+								fill='rgba(255, 255, 255, 0.96)'
+								color='rgba(255, 255, 255, 0.96)'
+							/>
+						</button>
+					</div>
 				</div>
-				<button
-					ref={ref}
-					style={{ alignSelf: 'center' }}
-					onClick={handleOnMoreOptions}
-				>
-					<Ellipsis
-						fill='rgba(255, 255, 255, 0.96)'
-						color='rgba(255, 255, 255, 0.96)'
-					/>
-				</button>
-			</div>
-		</div>
+			}
+		/>
+		// <div className={styles.scrollItemWrapper}>
+		// 	<div className={styles.scrollItemPosterWrapper}>
+		// 		<img
+		// 			src='https://static.okko.tv/images/v4/52c18c21-efc9-4c45-807f-476231a02cea?width=319&scale=1&quality=80&mediaType=webp'
+		// 			alt='cw poster'
+		// 			className={styles.scrollItemPoster}
+		// 		/>
+	
+		// 	</div>
+			
+		// </div>
 	);
 }
--- src/components/shared/movies-scroll/scroll-item.tsx
+++ src/components/shared/movies-scroll/scroll-item.tsx
@@ -7,7 +7,7 @@ import { getPosition } from '@/utils/getElementPosition';
 import { getSize } from '@/utils/getElementSize';
 import { useRef } from 'react';
 import { MoreModal } from '../more-modal';
-import styles from './scroll-item.module.css';
+import { Card } from '../card';
 
 export function ScrollItem() {
 	const itemRef = useRef<HTMLDivElement>(null);
@@ -15,10 +15,10 @@ export function ScrollItem() {
 	const isMobile = useCheckMobileScreen();
 	const { showModal } = useModal();
 	return (
-		<div
-			className={styles.scrollItemWrapper}
+		<Card
 			ref={itemRef}
 			onMouseOver={() => {
+
 				if (isMobile) return;
 
 				if (!itemRef?.current) {
@@ -34,18 +34,12 @@ export function ScrollItem() {
 					top:
 						getPosition(itemRef.current).top +
 						getSize(itemRef.current).height / 2,
-				});
+					});
 			}}
-			onClick={() => {
-				if (!isMobile) return;
-				showModal(<MoreModal />);
+			onClick={()=>{
+				if(!isMobile) return;
+				showModal(<MoreModal/>)
 			}}
-		>
-			<img
-				src='https://static.okko.tv/images/v4/52c18c21-efc9-4c45-807f-476231a02cea?width=319&scale=1&quality=80&mediaType=webp'
-				alt='cw poster'
-				className={styles.scrollItemPoster}
-			/>
-		</div>
+		/>
 	);
 }
