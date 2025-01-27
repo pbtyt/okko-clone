a68a74d403106e88d30bc7fcdf2dc8d458bc25e2
pbtyt <darkyt9@gmail.com>
2025-01-28 00:18:01 +0300

    fix: useSearchParams() should be wrapped in a suspense boundary at page /edit-profiles

--- src/app/edit-profiles/page.tsx
+++ src/app/edit-profiles/page.tsx
@@ -1,5 +1,6 @@
 import { NO_INDEX_PAGE } from '@/constants/seo.constants';
 import { Metadata } from 'next';
+import { Suspense } from 'react';
 import { SelectView } from './_components/select-view';
 import styles from './edit-profiles.module.css';
 
@@ -12,7 +13,11 @@ export default function EditProfilePage() {
 	return (
 		<main className={styles.main}>
 			<div>
-				<SelectView />
+				<Suspense
+					fallback={<div>Loading... (will appear in future updates)</div>}
+				>
+					<SelectView />
+				</Suspense>
 			</div>
 		</main>
 	);
